79ee03c35bb7b63a84a4526cb191b36eb53089a5
Taina Conde <tainabatistaconde@gmail.com>
2022-08-17 19:24:24 -0400

    delete runnable code from Concentration

--- src/main/java/cs665/tbconde/finalProject/structuralPatterns/composite/Concentration.java
+++ src/main/java/cs665/tbconde/finalProject/structuralPatterns/composite/Concentration.java
@@ -30,32 +30,5 @@ public class Concentration extends CourseComposite {
         }
         return null;
     }
-    public static void main(String[] args) {
-        CourseComposite course1 = new Course(
-                "Design patterns",
-                new FullTimeFaculty("kalanthur"),
-                "This course is about design patterns",
-                "Module 1 - creational patterns, Module 2 - structural patterns, Module 3 - behavioral patterns");
-        course1.format();
-        CourseComposite course2 = new Course(
-                "Software Engineering",
-                new FullTimeFaculty("Yuting"),
-                "This course is about ...",
-                "Module 1 - ..., Module 2 - ..., Module 3 - ...");
-        CourseComposite course3 = new Course(
-                "Python",
-                new PartTimeFaculty("bbb"),
-                "This course is about python",
-                "Module 1 - types, Module 2 - arrays, Module 3 - dictionary");
-        CourseComposite concentration1 = new Concentration("Software Engineering", "This concentration ...");
-        concentration1.add(course1);
-        concentration1.add(course2);
-        concentration1.format();
-        CourseComposite concentration2 = new Concentration("Procedural languages", "This subconcentration ...");
-        concentration2.add(course3);
-        concentration2.format();
-        CourseComposite concentration3 = new Concentration("Programming languages", "this concentration ...");
-        concentration3.add(concentration2);
-        concentration3.format();
-    }
+    
 }
